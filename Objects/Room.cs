03dc0b3acfb39e3af33cb1b02728be8700f2d066
Nicholas_Baxley <nickbax28@gmail.com>
2021-09-21 19:05:00 -0400

    Rooms and mobs have difficulty. Rooms have ids and exits.

--- Objects/Room.cs
+++ Objects/Room.cs
@@ -11,19 +11,30 @@ namespace Objects
         public string name { get; set; }
         public string desc { get; set; }
         public bool locked { get; set; }
+        public int id { get; set; }
+        // The exits should be the ids of the room they connect to
+        public int nExit { get; set; }
+        public int sExit { get; set; }
+        public int wExit { get; set; }
+        public int eExit { get; set; }
 
         public Room()
         {
             name = "Generic Room";
             desc = "Nothing in here!";
             locked = false;
+            id = 0;
         }
 
-        public Room(string roomName, string roomDesc, bool roomLocked = false)
+
+        ///add e exit here
+        ///finish adding room ids
+        public Room(string roomName, string roomDesc, bool roomLocked = false, int roomId)
         {
             name = roomName;
             desc = roomDesc;
             locked = roomLocked;
+            id = roomId;
         }
     }
 }
